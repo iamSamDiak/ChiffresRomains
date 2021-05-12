b0eb345f3f2cfac6654f0006884eb2ae269e3423
Sam Diak <thediakarthur@gmail.com>
2021-05-13 00:08:33 +0200

    Delete src directory

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const app = express();
-
-router.get("/", function(req, res){
-  res.render("index.html");
-});
-
-module.exports = router;
